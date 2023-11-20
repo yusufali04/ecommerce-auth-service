919274c66c6fdfcd61a8f1330b9b61640bdb7724
Yusuf <yusufali.5094@gmail.com>
2023-11-20 22:18:32 +0530

    setup error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res) => {
+    const err = createHttpError(401, "Unauthorized person");
+    throw err;
     res.send("welcome to auth service");
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
