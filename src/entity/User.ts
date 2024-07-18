0b44d9387cfea6ba6d857bc96536ca441a2029fc
Yusuf <yusufali.5094@gmail.com>
2024-07-18 12:24:20 +0530

    setup database

---
+++ src/entity/User.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    firstName: string;
+    @Column()
+    lastName: string;
+    @Column()
+    email: string;
+    @Column()
+    password: string;
+}
