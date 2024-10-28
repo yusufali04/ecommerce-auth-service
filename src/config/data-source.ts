22b0d7811ed060bf1a87c1fd58f4563d67a361aa
Yusuf <yusufali.5094@gmail.com>
2024-10-28 23:09:47 +0530

    add tenants table

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,8 +1,6 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "../entity/User";
 import { Config } from ".";
-import { RefreshToken } from "../entity/RefreshToken";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -13,7 +11,7 @@ export const AppDataSource = new DataSource({
     database: Config.DB_NAME,
     synchronize: false,
     logging: false,
-    entities: [User, RefreshToken],
+    entities: ["src/entity/*.ts"],
     migrations: ["src/migration/*.ts"],
     subscribers: [],
 });
