6fedd210a72a2773c393e6c6b1967b1e85ac3619
Yusuf <yusufali.5094@gmail.com>
2025-07-20 14:53:07 +0530

    remove comments and duplicates

--- src/middlewares/validateRefreshToken.ts
+++ src/middlewares/validateRefreshToken.ts
@@ -1,11 +1,10 @@
 import { expressjwt } from "express-jwt";
 import { Config } from "../config";
 import { Request } from "express";
-import { AuthCookie } from "../types";
+import { AuthCookie, IRefreshTokenPayload } from "../types";
 import { AppDataSource } from "../config/data-source";
 import { RefreshToken } from "../entity/RefreshToken";
 import logger from "../config/logger";
-import { IRefreshTokenPayload } from "../types";
 
 export default expressjwt({
     secret: Config.REFRESH_TOKEN_SECRET!,
--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -1,6 +1,5 @@
 import { checkSchema } from "express-validator";
 
-// export default [body('email').notEmpty().withMessage('Email is required')];
 export default checkSchema({
     email: {
         errorMessage: "Email is required",
